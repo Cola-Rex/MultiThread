a5148c538388b6036f5849e75a3fe69c1d852e21
Ezreal <1649811141@qq.com>
2018-10-15 09:42:36 +0800

    CyclicBarrier构造函数增加 runnable 参数

--- src/main/java/concurrentUtil/CyclicBarrierExample.java
+++ src/main/java/concurrentUtil/CyclicBarrierExample.java
@@ -10,7 +10,12 @@ public class CyclicBarrierExample {
 
 	public static void main(String[] args) {
 		int N = 4;
-		CyclicBarrier barrier = new CyclicBarrier(N);
+		CyclicBarrier barrier = new CyclicBarrier(N, new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("当前线程" + Thread.currentThread().getName());
+			}
+		});
 		for (int i = 0; i < N; i++) {
 			new Thread(new Writer(barrier)).start();
 		}
@@ -27,7 +32,7 @@ public class CyclicBarrierExample {
 		public void run() {
 			System.out.println("线程"+Thread.currentThread().getName()+"正在写入数据...");
 			try {
-				Thread.sleep(5000);
+				Thread.sleep(1000);
 				System.out.println("线程" + Thread.currentThread().getName() + "写入数据完毕，等待其他线程写入完毕");
 				cyclicBarrier.await();
 			} catch (InterruptedException e) {
